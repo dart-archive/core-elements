01826300270359d0074ce16dfff5ced505d71d0e
John Messerly <jmesserly@google.com>
2014-06-22 19:15:02 -0700

    add core-item, core-menu*

---
+++ lib/core_menu/core_menu.dart
@@ -0,0 +1,57 @@
+// DO NOT EDIT: auto-generated with core_elements/tool/generate_dart_api.dart
+
+/// Dart API for the polymer element `core-menu`.
+library core_elements.core_menu;
+
+import 'dart:html';
+import 'dart:js' show JsArray;
+import 'package:web_components/interop.dart' show registerDartType;
+import 'package:polymer/polymer.dart' show initMethod;
+import 'package:core_elements/core_selector/core_selector.dart';
+
+/// `core-menu` is a selector which styles to looks like a menu.
+///
+///     <core-menu selected="0">
+///       <core-item icon="settings" label="Settings"></core-item>
+///       <core-item icon="dialog" label="Dialog"></core-item>
+///       <core-item icon="search" label="Search"></core-item>
+///     </core-menu>
+///
+/// When an item is selected the `core-selected` class is added to it.  The user can
+/// use the class to add more stylings to the selected item.
+///
+///     core-item.core-selected {
+///       color: red;
+///     }
+///
+/// The `selectedItem` property references the selected item.
+///
+///     <core-menu selected="0" selectedItem="{{item}}">
+///       <core-item icon="settings" label="Settings"></core-item>
+///       <core-item icon="dialog" label="Dialog"></core-item>
+///       <core-item icon="search" label="Search"></core-item>
+///     </core-menu>
+///
+///     <div>selected label: {{item.label}}</div>
+///
+/// The `core-select` event signals selection change.
+///
+///     <core-menu selected="0" on-core-select="{{selectAction}}">
+///       <core-item icon="settings" label="Settings"></core-item>
+///       <core-item icon="dialog" label="Dialog"></core-item>
+///       <core-item icon="search" label="Search"></core-item>
+///     </core-menu>
+///
+///     ...
+///
+///     selectAction: function(e, detail) {
+///       if (detail.isSelected) {
+///         var selectedItem = detail.item;
+///         ...
+///       }
+///     }
+class CoreMenu extends CoreSelector {
+  CoreMenu.created() : super.created();
+}
+@initMethod
+upgradeCoreMenu() => registerDartType('core-menu', CoreMenu);
