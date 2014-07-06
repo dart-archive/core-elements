746af52a65f991fa7d17501cc9621b51eeb46e96
zoechi <gzoechi@gmail.com>
2014-07-06 13:49:35 +0200

    #41 core-ajax-dart unittest (fails with `<debug target crashed>`

---
+++ test/core_ajax.dart
@@ -0,0 +1,31 @@
+//Copyright (c) 2014 The Polymer Project Authors. All rights reserved.
+//This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+//The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+//The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+//Code distributed by Google as part of the polymer project is also
+//subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+
+library core_ajax.test;
+
+import "dart:html";
+import "package:polymer/polymer.dart";
+import "package:unittest/unittest.dart";
+import "package:unittest/html_enhanced_config.dart";
+
+@initMethod
+void init() {
+//  useHtmlEnhancedConfiguration();
+//
+//  initPolymer().run(() {
+//    Polymer.onReady.then((_) {
+//      test("core-ajax-dart", () {
+//        var done = expectAsync(() {});
+//        var s = document.querySelector("core-ajax-dart");
+//        s.addEventListener("core-response", (event) {
+//          expect(event.detail['response']['feed']['entry'].length, greaterThan(0));
+//          done();
+//        });
+//      });
+//    });
+//  });
+}
