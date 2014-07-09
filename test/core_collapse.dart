3ff4fc7a8fce110a4ae143116350817c07385213
zoechi <gzoechi@gmail.com>
2014-07-09 13:00:36 +0200

    update unit test according to the comments in #43

--- test/core_collapse.dart
+++ test/core_collapse.dart
@@ -11,14 +11,11 @@ import "dart:html" as dom;
 import "dart:async" as async;
 import "package:polymer/polymer.dart";
 import "package:unittest/unittest.dart";
-import "package:unittest/html_enhanced_config.dart" show
-    useHtmlEnhancedConfiguration;
-import "package:core_elements/core_collapse.dart" show
-    CoreCollapse;
+import "package:unittest/html_config.dart" show useHtmlConfiguration;
+import "package:core_elements/core_collapse.dart" show CoreCollapse;
 
-@initMethod
 void main() {
-  useHtmlEnhancedConfiguration();
+  useHtmlConfiguration();
 
   test("core-collapse", () {
     dom.querySelector("button").onClick.listen((e) =>
@@ -26,28 +23,28 @@ void main() {
 
     Duration delay = new Duration(milliseconds: 200);
 
-    Polymer.onReady.then((_) {
-      var done = expectAsync(() {});
-      async.Timer.run(() {
-        var c = dom.document.querySelector("#collapse") as CoreCollapse;
-        // verify take attribute for opened is correct
-        expect(c.opened, isTrue);
-        new async.Future.delayed(delay, () {
-          // get the height for the opened state
-          var h = getCollapseComputedStyle().height;
-          // verify the height is not 0px
-          expect(h, isNot(equals("0px")));
-          // close it
-          c.opened = false;
-          new async.Future.delayed(delay, () {
-            // verify is closed
-            expect(getCollapseComputedStyle().height, isNot(equals(h)));
-            // open it
-            c.opened = true;
-            new async.Future.delayed(delay, () {
-              // verify is opened
-              expect(getCollapseComputedStyle().height, equals(h));
-              done();
+    return initPolymer().run(() {
+      return Polymer.onReady.then((_) {
+        return new async.Future(() {
+          var c = dom.document.querySelector("#collapse") as CoreCollapse;
+          // verify take attribute for opened is correct
+          expect(c.opened, isTrue);
+          return new async.Future.delayed(delay, () {
+            // get the height for the opened state
+            var h = getCollapseComputedStyle().height;
+            // verify the height is not 0px
+            expect(h, isNot(equals("0px")));
+            // close it
+            c.opened = false;
+            return new async.Future.delayed(delay, () {
+              // verify is closed
+              expect(getCollapseComputedStyle().height, isNot(equals(h)));
+              // open it
+              c.opened = true;
+              return new async.Future.delayed(delay, () {
+                // verify is opened
+                expect(getCollapseComputedStyle().height, equals(h));
+              });
             });
           });
         });
