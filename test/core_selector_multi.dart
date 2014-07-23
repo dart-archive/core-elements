46e7bb3f9eb7a69bfb0713a1c4acb410052bb062
zoechi <gzoechi@gmail.com>
2014-07-23 16:30:10 +0200

    remove workaround for #52 (missing `item` getter) fix quotes

--- test/core_selector_multi.dart
+++ test/core_selector_multi.dart
@@ -7,13 +7,13 @@
 
 library core_selector.test.multi;
 
-import "dart:async" as async;
-import "dart:html" as dom;
-import "dart:js" as js;
-import "package:polymer/polymer.dart";
-import "package:unittest/unittest.dart";
-import "package:unittest/html_config.dart" show useHtmlConfiguration;
-import "package:core_elements/core_selector.dart" show CoreSelector;
+import 'dart:async' as async;
+import 'dart:html' as dom;
+import 'dart:js' as js;
+import 'package:polymer/polymer.dart';
+import 'package:unittest/unittest.dart';
+import 'package:unittest/html_config.dart' show useHtmlConfiguration;
+import 'package:core_elements/core_selector.dart' show CoreSelector;
 
 void oneMutation(dom.Element node, options, Function cb) {
   var o = new dom.MutationObserver((List<dom.MutationRecord>
@@ -21,7 +21,7 @@ void oneMutation(dom.Element node, options, Function cb) {
     cb();
     observer.disconnect();
   });
-  o.observe(node, attributes: options["attributes"]);
+  o.observe(node, attributes: options['attributes']);
 }
 
 void main() {
@@ -30,23 +30,22 @@ void main() {
   initPolymer().run(() {
     Polymer.onReady.then((e) {
 
-      group("core-selector", () {
+      group('core-selector', () {
 
-        test("core-selector-multi", () {
+        test('core-selector-multi', () {
           // selector1
-          var s = (dom.document.querySelector("#selector") as CoreSelector);
+          var s = (dom.document.querySelector('#selector') as CoreSelector);
           expect(s.selected, isNull);
-          expect(s.selectedClass, equals("core-selected"));
+          expect(s.selectedClass, equals('core-selected'));
           expect(s.multi, isTrue);
-          expect(s.valueattr, equals("name"));
-          // TODO(zoechi) expect(s.items.length, equals(5)); // getter items not available, see #52
-          expect(s.jsElement['items']['length'], equals(5));
+          expect(s.valueattr, equals('name'));
+          expect(s.items.length, equals(5));
           // setup listener for polymer-select event
           var selectEventCounter = 0;
-          s.on["core-select"].listen((dom.CustomEvent e) {
+          s.on['core-select'].listen((dom.CustomEvent e) {
             // TODO(zoechi)event detail is null https://code.google.com/p/dart/issues/detail?id=19315
             var detail = new js.JsObject.fromBrowserObject(e)['detail'];
-            if (detail["isSelected"]) {
+            if (detail['isSelected']) {
               selectEventCounter++;
             } else {
               selectEventCounter--;
@@ -60,17 +59,17 @@ void main() {
             // check polymer-select event
             expect(selectEventCounter, equals(2));
             // check selected class
-            expect(s.children[0].classes.contains("core-selected"), isTrue);
-            expect(s.children[2].classes.contains("core-selected"), isTrue);
+            expect(s.children[0].classes.contains('core-selected'), isTrue);
+            expect(s.children[2].classes.contains('core-selected'), isTrue);
             // check selectedItem
             expect(s.selectedItem.length, equals(2));
             expect(s.selectedItem[0], equals(s.children[0]));
             expect(s.selectedItem[1], equals(s.children[2]));
             // tap on already selected element should unselect it
-            s.children[0].dispatchEvent(new dom.CustomEvent("tap", canBubble: true));
+            s.children[0].dispatchEvent(new dom.CustomEvent('tap', canBubble: true));
             // check selected
             expect(s.selected.length, equals(1));
-            expect(s.children[0].classes.contains("core-selected"), isFalse);
+            expect(s.children[0].classes.contains('core-selected'), isFalse);
           });
         });
 
